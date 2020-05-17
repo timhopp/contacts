2e8d597fc33fe74877f955a9f090d8fcd3507471
tim hopp <timothyjhopp@gmail.com>
2020-05-17 15:01:21 -0600

    removed comments

--- main.js
+++ main.js
@@ -1,14 +1,5 @@
 let contacts = []
 
-/**
- * Called when submitting the new Contact Form
- * This method will pull data from the form
- * use the provided function to give the data an id
- * then add that data to the contacts list.
- * Then reset the form
- * *** hints:
- * *** push: resources/push.jpg
- */
 function addContact(event) {
 event.preventDefault();
 let form = event.target;
@@ -24,10 +15,6 @@ saveContacts();
 form.reset();
 }
 
-/**
- * Converts the contacts array to a JSON string then
- * Saves the string to localstorage at the key contacts 
- */
 function saveContacts() {
   window.localStorage.setItem("contacts", JSON.stringify(contacts));
   drawContacts();
@@ -45,11 +32,6 @@ function loadContacts() {
  } 
 }
 
-/**
- * This function targets the contacts-list on the 
- * DOM and adds a new div element for each of the
- * contacts in the contacts array
- */
 function drawContacts() {
   let contactListElement = document.getElementById("contact-list")
   let contactsTemplate = ""
@@ -70,15 +52,8 @@ function drawContacts() {
  contactListElement.innerHTML = contactsTemplate
 }
 
-/**
- * This function is called with a contact id
- * and will use the id to find and remove the 
- * contact by their id from the list of contacts
- * *** hints: 
- * *** findIndex: resources/findIndex.jpg
- * *** splice: resources/splice.jpg
- * @param {string} contactId 
- */
+
+
 function removeContact(contactId) {
   let index = contacts.findIndex(contact => contact.id == contactId)
   if(index == -1){
@@ -88,9 +63,6 @@ function removeContact(contactId) {
   saveContacts();
 }
 
-/**
- * Toggles the visibility of the AddContact Form
- */
 
 function toggleAddContactForm() {
   document.getElementById("new-contact-form").classList.toggle("hidden");
@@ -98,11 +70,6 @@ function toggleAddContactForm() {
 }
 
 
-/**
- * Used to generate a random string id for mocked
- * database generated Id
- * @returns {string}
- */
 function generateId() {
   return Math.floor(Math.random() * 10000000) + "-" + Math.floor(Math.random() * 10000000)
 }
